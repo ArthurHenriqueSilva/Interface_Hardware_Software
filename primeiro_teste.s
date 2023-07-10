ac86dfc9d78c3718f2c971d03fb860814df6b6aa
Arthur Henrique Silva de Lima Arthur <115894179+ArthurHenriqueSilva@users.noreply.github.com>
2023-07-10 17:27:41 -0300

    Update primeiro_teste.s

--- primeiro_teste.s
+++ primeiro_teste.s
@@ -1,31 +1,48 @@
 .intel_syntax noprefix
 
 .section .text
-; Main function
+// Main function
 .global main
 main:
-    ; registro de pilha
+    // registro de pilha
     push rbp
     mov rbp, rsp
+    //reserva memoria na pilha
+    sub rsp, 16
 
-    ; ler nome do arquivo_entrada(argv[1])
-    mov r12, rsi ; guardar argv
-    mov r13, [rsi+8] ; guarda valor de argv[1]
-    lea rdi, [rip + leitura] ; argumento 1 para fopen
-    mov rsi, r13 ; argumento 2 para fopen
+    
+    // ler nome do arquivo_entrada(argv[1])
+    // guardar argv
+    mov r12, rsi
+    // guarda valor de argv[1]
+    mov r13, [rsi+8]
+    // argumento 1 para fopen
+    lea rdi, [rip + leitura] 
+    // argumento 2 para fopen
+    mov rsi, r13 
+    
 
-    ; abrir arquivo_entrada
+    // abrir arquivo_entrada
     call fopen@plt
-    mov r14, rax ; guarda ponteiro do arquivo
-    mov rdi, r14 ; argumento 1 para fscanf
-    lea rsi, [rip + inteiro] ; argumento 2 para fscanf
+    // guarda ponteiro do arquivo
+    mov r14, rax
+    // argumento 1 para fscanf
+    mov rdi, r14
+    // argumento 2 para fscanf
+    lea rsi, [rip + inteiro] 
     call fscanf@plt
-    mov r15, rax ; guarda ponteiro de dentro do arquivo
-    mov r16, 1 ; ancora para leitura (ptr + r16*4)
-    shl r16, 2 ; r16*4
-    mov r17, [r15 + r16] ; r17 = file[r16], leitura primeiro inteiro do arquivo
-    lea rsi, [rip + inteiro] ; argumento 1 printf
-    mov rdi, rdi
+    // guarda ponteiro de dentro do arquivo. r15 = ptr
+    mov r15, rax
+    // ancora para leitura (ptr + r16*4)
+    mov r16, 1
+    // r16*4
+    shl r16, 2
+    // r17 = file[r16], leitura primeiro inteiro do arquivo
+    mov r17, [r15 + r16]
+    // argumento 1 printf
+    mov rdi, r17
+    //argumento 2 printf
+    lea rsi, [rip + inteiro] 
     call printf@plt
 
 
