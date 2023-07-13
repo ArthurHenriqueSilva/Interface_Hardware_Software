bf9ac27a88f328468010a8da7f355f44b67bed1a
Arthur Henrique Silva de Lima Arthur <115894179+ArthurHenriqueSilva@users.noreply.github.com>
2023-07-13 13:11:07 -0300

    Update primeiro_teste.s

--- primeiro_teste.s
+++ primeiro_teste.s
@@ -26,18 +26,23 @@ main:
     // argumento 1 para fscanf
     mov rdi, r14
     // argumento 2 para fscanf
-    lea rsi, [rip + inteiro] 
+    lea rsi, [rip + inteiro]
+    lea rdx, [rbp - 16]
     call fscanf@plt
     // guarda ponteiro de dentro do arquivo. r15 = ptr
     mov r15, rdx
     // ancora para leitura (ptr + r16*4)
     
     // argumento 1 printf
-    lea rdi, dword ptr[r15 + 4]   
+    lea rsi, [r15 + 4]   
     //argumento 2 printf
-    lea rsi, [rip + inteiro] 
+    lea rdi, [rip + inteiro] 
     call printf@plt
-
+    epilogue:
+        mov rsp, rbp
+        pop rbp
+        ret
+        
 .section .rodata
 leitura:
     .string "r\n"
