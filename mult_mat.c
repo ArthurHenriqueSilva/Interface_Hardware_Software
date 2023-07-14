6c2cb6f2383432b3bdfb52856a69933fc92b7cfb
Arthur Henrique Silva de Lima Arthur <115894179+ArthurHenriqueSilva@users.noreply.github.com>
2023-07-14 18:07:07 -0300

    Update mult_mat.c

--- mult_mat.c
+++ mult_mat.c
@@ -3,25 +3,29 @@
 #include <pthread.h>
 
 typedef struct {
-    int matrixSize;
     int matrixIndex;
+    int numRowsA;
+    int numColsA;
+    int numColsB;
     int** matrixA;
     int** matrixB;
 } MatrixData;
 
 void* matrixMultiply(void* arg) {
     MatrixData* data = (MatrixData*)arg;
-    int size = data->matrixSize;
     int index = data->matrixIndex;
+    int numRowsA = data->numRowsA;
+    int numColsA = data->numColsA;
+    int numColsB = data->numColsB;
     int** matrixA = data->matrixA;
     int** matrixB = data->matrixB;
-    int** matrixC = (int**)malloc(size * sizeof(int*));
+    int** matrixC = (int**)malloc(numRowsA * sizeof(int*));
 
-    for (int i = 0; i < size; i++) {
-        matrixC[i] = (int*)malloc(size * sizeof(int));
-        for (int j = 0; j < size; j++) {
+    for (int i = 0; i < numRowsA; i++) {
+        matrixC[i] = (int*)malloc(numColsB * sizeof(int));
+        for (int j = 0; j < numColsB; j++) {
             matrixC[i][j] = 0;
-            for (int k = 0; k < size; k++) {
+            for (int k = 0; k < numColsA; k++) {
                 matrixC[i][j] += matrixA[i][k] * matrixB[k][j];
             }
         }
@@ -36,15 +40,15 @@ void* matrixMultiply(void* arg) {
     }
 
     fprintf(outputFile, "M%d:\n", index);
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
+    for (int i = 0; i < numRowsA; i++) {
+        for (int j = 0; j < numColsB; j++) {
             fprintf(outputFile, "%d ", matrixC[i][j]);
         }
         fprintf(outputFile, "\n");
     }
 
     fclose(outputFile);
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < numRowsA; i++) {
         free(matrixC[i]);
     }
     free(matrixC);
@@ -66,24 +70,26 @@ int main() {
     MatrixData* matrixData = (MatrixData*)malloc(numMultiplications * sizeof(MatrixData));
 
     for (int i = 0; i < numMultiplications; i++) {
-        int size;
-        fscanf(inputFile, "%d", &size);
+        int numRowsA, numColsA, numColsB;
+        fscanf(inputFile, "%d %d %d", &numRowsA, &numColsA, &numColsB);
 
-        matrixData[i].matrixSize = size;
         matrixData[i].matrixIndex = i + 1;
-        matrixData[i].matrixA = (int**)malloc(size * sizeof(int*));
-        matrixData[i].matrixB = (int**)malloc(size * sizeof(int*));
+        matrixData[i].numRowsA = numRowsA;
+        matrixData[i].numColsA = numColsA;
+        matrixData[i].numColsB = numColsB;
+        matrixData[i].matrixA = (int**)malloc(numRowsA * sizeof(int*));
+        matrixData[i].matrixB = (int**)malloc(numColsA * sizeof(int*));
 
-        for (int j = 0; j < size; j++) {
-            matrixData[i].matrixA[j] = (int*)malloc(size * sizeof(int));
-            for (int k = 0; k < size; k++) {
+        for (int j = 0; j < numRowsA; j++) {
+            matrixData[i].matrixA[j] = (int*)malloc(numColsA * sizeof(int));
+            for (int k = 0; k < numColsA; k++) {
                 fscanf(inputFile, "%d", &matrixData[i].matrixA[j][k]);
             }
         }
 
-        for (int j = 0; j < size; j++) {
-            matrixData[i].matrixB[j] = (int*)malloc(size * sizeof(int));
-            for (int k = 0; k < size; k++) {
+        for (int j = 0; j < numColsA; j++) {
+            matrixData[i].matrixB[j] = (int*)malloc(numColsB * sizeof(int));
+            for (int k = 0; k < numColsB; k++) {
                 fscanf(inputFile, "%d", &matrixData[i].matrixB[j][k]);
             }
         }
@@ -104,11 +110,13 @@ int main() {
     fclose(inputFile);
     free(threads);
     for (int i = 0; i < numMultiplications; i++) {
-        for (int j = 0; j < matrixData[i].matrixSize; j++) {
+        for (int j = 0; j < matrixData[i].numRowsA; j++) {
             free(matrixData[i].matrixA[j]);
-            free(matrixData[i].matrixB[j]);
         }
         free(matrixData[i].matrixA);
+        for (int j = 0; j < matrixData[i].numColsA; j++) {
+            free(matrixData[i].matrixB[j]);
+        }
         free(matrixData[i].matrixB);
     }
     free(matrixData);
