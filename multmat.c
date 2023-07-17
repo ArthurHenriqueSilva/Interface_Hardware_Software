b1a343cc33af8633a04c4f276a745c6c0ad0d159
arthur <arthur@Ubuntu.myguest.virtualbox.org>
2023-07-17 19:24:16 -0300

    to fix

--- multmat.c
+++ multmat.c
@@ -2,7 +2,41 @@
 #include <stdlib.h>
 #include <pthread.h>
 
+typedef struct{
+	int n1, m1, n2, m2;
+	double** matriz1, matriz2;
+}MatrixData;
 
+typedef struct{
+	int c, n, m;
+	double** result;
+}ThreadData;
+
+void* multmx(void* args){
+	ThreadData* td = (ThreadData*)args;
+	int c = td->c;
+	int n  = td->n;
+	int m = td->m;
+	double** result = td->result;
+
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < m; j++){
+			result[i][j] = 0;
+			for(int k = 0; k < n; k++){
+				result[i][j] = td->matriz1[i][k] * td->matriz2[k][j];
+
+			}
+		}
+	}
+	printf("M%d:\n", c);
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < m; j++){
+			printf("%.2lf", result[i][j]);
+		}
+		printf("\n");
+	}
+	pthread_exit(NULL);
+}
 int main(int argc, char *argv[]) {
 	printf("Programa comecou!\n");
 	printf("Arquivo de entrada = %s\n", argv[1]);
@@ -10,42 +44,69 @@ int main(int argc, char *argv[]) {
 	int num_iter;
 	fscanf(input, "%d", &num_iter);
 	printf("num_iter = %d\n", num_iter);
+	
+	pthread_t* threads = (pthread_t*)malloc(num_iter * sizeof(pthread_t));
+	MatrixData* md = (Matrixdata*)malloc(num_iter*sizeof(MatrixData));
 	for(int c = 0; c < num_iter; c++){
-		int n1, m1;
-		fscanf(input, "%d %d", &n1, &m1);
-		printf("%d %d\n", n1, m1);
-		double **matriz1 = (double **)malloc(n1 * sizeof(double *));
-		for(int i = 0; i < n1; i++ ){
-			matriz1[i] = (double *)malloc(m1 * sizeof(double));
-			for(int j = 0; j < m1; j++){
-				fscanf(input, "%lf", &matriz1[i][j]);
+		fscanf(input, "%d %d", md[c].n1, md[c].m1);
+		printf("%d %d\n", md[c].n1, md[c].m1);
+		md[c].matriz1 = (double**)malloc(md[c].n1 * sizeof(double*));
+		for(int i = 0; i < md[c].n1; i++){
+			md[c].matriz1[i] = (double*)malloc(md[c].m1 * sizeof(double));
+			for(int j = 0; j < md[c].m1; j++){
+				fscanf(input, "%lf", &md[c].matriz1[i][j]);
 			}
 		}
 
-		int n2, m2;
-		fscanf(input, "%d %d", &n2, &m2);
-		printf("%d %d\n", n2, m2);
-		double **matriz2 = (double **)malloc(n2 * sizeof(double *));
-		for(int i = 0; i < n2; i++){
-			matriz2[i] = (double *)malloc(m2 * sizeof(double));
-			for(int j = 0; j < m2; j++){
-				fscanf(input, "%lf", &matriz2[i][j]);
+		fscanf(input, "%d %d", &md[c].n2, &md[c].m2);
+		printf("%d %d\n", md[c].n2, md[c].m2);
+		md[c].matriz2 = (double**)malloc(md[c].n2 * sizeof(double*));
+		for(int i = 0; i < md[c].n2; i++){
+			md[c].matriz2[i] = (double*)malloc(md[c].m2 * sizeof(double));
+			for(int j = 0; j < md[c].m2; j++){
+				fscanf(input, "%lf", &md[c].matriz2[i][j]);
 			}
 		}
+		
+		int n = md[c].n1;
+		int m = md[c].m2;
+		double** result = (double**)malloc(n * sizeof(double*));
+		for(int i = 0; i < n; i++){
+			result[i] = (double*)malloc(m * sizeof(double));
+		}
 
+		ThreadData* td = (ThreadData*)malloc(sizeof(Threaddata));
+		td->c = c;
+		td->n = n;
+		td->m = m;
+		td->matriz1 = md[c].matriz1;
+		td->matriz2 = md[c].matriz2;
+		td->result = result;
 		
-		
-		
-		for(int i = 0; i < n1; i++){
-			free(matriz1[i]);
-			free(matriz2[i]);
+		if(pthread_create(&threads[c], NULL, multmx, (void*)td) != 0){
+			fprintf(stderr, "Erro ao criar thread %d\n", c);
+			exit(EXIT_FAILURE);
 		}
-		free(matriz1);
-		for(int i = 0; i < n2; i++){
-			free(matriz2[i]);
-		}
-		free(matriz2);
 	}
+
+	for(int c = 0; c < num_iter; c++){
+		if(pthread_join(threads[c], NULL) != 0){
+			fprintf(stderr, "Erro no join da thread %d\n", c);
+		}
+	}
+	
 	fclose(input);
-	return 0;
+	free(threads);
+	for(int c = 0; c < num_iter; c++){
+		for(int i = 0; i < md[c].n1; n1++){
+			free(md[c].matriz1);
+		}
+		free(md[c].matriz1);
+		for(int i = 0; i < md[c].n2; i++){
+			free(md[c].matriz2[i]);
+		}
+		free(md[c].matriz2);
+	}
+	free(md);
+	return 0
 }
