43621d8ec62adde8805f6ce7f9f5bc09d3bf8779
Arthur Henrique Silva de Lima Arthur <115894179+ArthurHenriqueSilva@users.noreply.github.com>
2023-07-19 17:34:08 -0300

    Update multmat.c

--- multmat.c
+++ multmat.c
@@ -7,26 +7,26 @@ typedef struct {
     int n1, m1, n2, m2;
     double** matriz1;
     double** matriz2;
+    double** result;
 } MatrixData;
 
 typedef struct {
-    int c, start_row, end_row, start_col, end_col;
-    double** result;
-    double** matriz1;
-    double** matriz2;
+    int start_row, end_row, start_col, end_col;
+    MatrixData* md;
 } ThreadData;
 
-void* multmx(void* args) {
+void* mult_block(void* args) {
     ThreadData* td = (ThreadData*)args;
-    int r;
-    // Realiza a multiplicação das matrizes para a parte atribuída à thread
+    int n = td->md->n1;
+    int m = td->md->m2;
+
     for (int i = td->start_row; i < td->end_row; i++) {
         for (int j = td->start_col; j < td->end_col; j++) {
-            r = 0;
-            for (int k = td->start_row; k < td->end_row; k++) {
-                r += td->matriz1[i][k] * td->matriz2[k][j];
+            double r = 0;
+            for (int k = 0; k < td->md->m1; k++) {
+                r += td->md->matriz1[i][k] * td->md->matriz2[k][j];
             }
-            td->result[i][j] = r;
+            td->md->result[i][j] = r;
         }
     }
 
@@ -36,129 +36,126 @@ void* multmx(void* args) {
 int main(int argc, char* argv[]) {
     printf("Programa comecou!\n");
     FILE* input = fopen(argv[1], "r");
-    if (access(argv[1], R_OK) != 0) {
-        printf("Arquivo nao possui permissao de leitura.\n");
+    if (input == NULL) {
+        printf("Erro ao abrir o arquivo de entrada.\n");
         return 1;
     }
 
-    if (input == NULL) {
-        printf("FOPEN gerou ponteiro nulo!\n");
-        return 1;
-    }
-    printf("Arquivo de entrada = %s\n", argv[1]);
     int num_iter;
     fscanf(input, "%d", &num_iter);
     printf("num_iter = %d\n", num_iter);
 
-    pthread_t* threads = (pthread_t*)malloc(num_iter * sizeof(pthread_t));
-    MatrixData* md = (MatrixData*)malloc(num_iter * sizeof(MatrixData));
-
     for (int c = 0; c < num_iter; c++) {
-        fscanf(input, "%d %d", &md[c].n1, &md[c].m1);
-        printf("%d %d\n", md[c].n1, md[c].m1);
-        md[c].matriz1 = (double**)malloc(md[c].n1 * sizeof(double*));
-        for (int i = 0; i < md[c].n1; i++) {
-            md[c].matriz1[i] = (double*)malloc(md[c].m1 * sizeof(double));
-            for (int j = 0; j < md[c].m1; j++) {
-                fscanf(input, "%lf", &md[c].matriz1[i][j]);
+        MatrixData md;
+        fscanf(input, "%d %d", &md.n1, &md.m1);
+        printf("%d %d\n", md.n1, md.m1);
+        md.matriz1 = (double**)malloc(md.n1 * sizeof(double*));
+        for (int i = 0; i < md.n1; i++) {
+            md.matriz1[i] = (double*)malloc(md.m1 * sizeof(double));
+            for (int j = 0; j < md.m1; j++) {
+                fscanf(input, "%lf", &md.matriz1[i][j]);
             }
         }
 
-        fscanf(input, "%d %d", &md[c].n2, &md[c].m2);
-        printf("%d %d\n", md[c].n2, md[c].m2);
-        md[c].matriz2 = (double**)malloc(md[c].n2 * sizeof(double*));
-        for (int i = 0; i < md[c].n2; i++) {
-            md[c].matriz2[i] = (double*)malloc(md[c].m2 * sizeof(double));
-            for (int j = 0; j < md[c].m2; j++) {
-                fscanf(input, "%lf", &md[c].matriz2[i][j]);
+        fscanf(input, "%d %d", &md.n2, &md.m2);
+        printf("%d %d\n", md.n2, md.m2);
+        md.matriz2 = (double**)malloc(md.n2 * sizeof(double*));
+        for (int i = 0; i < md.n2; i++) {
+            md.matriz2[i] = (double*)malloc(md.m2 * sizeof(double));
+            for (int j = 0; j < md.m2; j++) {
+                fscanf(input, "%lf", &md.matriz2[i][j]);
             }
         }
 
-        int n = md[c].n1;
-        int m = md[c].m2;
-        double** result = (double**)malloc(n * sizeof(double*));
+        int n = md.n1;
+        int m = md.m2;
+        md.result = (double**)malloc(n * sizeof(double*));
         for (int i = 0; i < n; i++) {
-            result[i] = (double*)malloc(m * sizeof(double));
+            md.result[i] = (double*)malloc(m * sizeof(double));
         }
 
+        // Número eficiente de threads (número de núcleos do processador)
         int num_threads = sysconf(_SC_NPROCESSORS_ONLN);
         printf("num_threads %d\n", num_threads);
-        int rows_per_thread = n / num_threads;
-        int cols_per_thread = m / num_threads;
-        int remaining_rows = n % num_threads;
-        int remaining_cols = m % num_threads;
 
+        // Aloca espaço para as threads e dados específicos de cada thread
+        pthread_t* threads = (pthread_t*)malloc(num_threads * sizeof(pthread_t));
         ThreadData* td = (ThreadData*)malloc(num_threads * sizeof(ThreadData));
+
+        // Calcula o tamanho dos blocos para cada thread
+        int block_size = n / num_threads;
+        int remaining_blocks = n % num_threads;
+
+        // Criação das threads
+        int start_row = 0;
         for (int t = 0; t < num_threads; t++) {
-            td[t].c = c;
-            td[t].start_row = t * rows_per_thread;
-            td[t].end_row = (t + 1) * rows_per_thread;
-            td[t].start_col = t * cols_per_thread;
-            td[t].end_col = (t + 1) * cols_per_thread;
+            td[t].start_row = start_row;
+            td[t].end_row = start_row + block_size;
 
             if (t == num_threads - 1) {
-                td[t].end_row += remaining_rows;
-                td[t].end_col += remaining_cols;
+                td[t].end_row += remaining_blocks;
             }
-            td[t].result = result;
-            td[t].matriz1 = md[c].matriz1;
-            td[t].matriz2 = md[c].matriz2;
+            td[t].start_col = 0;
+            td[t].end_col = m;
+            td[t].md = &md;
 
-            if (pthread_create(&threads[t], NULL, multmx, (void*)&td[t]) != 0) {
+            if (pthread_create(&threads[t], NULL, mult_block, (void*)&td[t]) != 0) {
                 fprintf(stderr, "Erro ao criar thread %d\n", t);
                 exit(EXIT_FAILURE);
             }
+
+            start_row = td[t].end_row;
         }
 
-        printf("Matriz A:\n");
-        for (int i = 0; i < md[c].n1; i++) {
-            for (int j = 0; j < md[c].m1; j++) {
-                printf("%.2lf ", md[c].matriz1[i][j]);
-            }
-            printf("\n");
-        }
-
-        printf("Matriz B:\n");
-        for (int i = 0; i < md[c].n2; i++) {
-            for (int j = 0; j < md[c].m2; j++) {
-                printf("%.2lf ", md[c].matriz2[i][j]);
-            }
-            printf("\n");
-        }
-
+        // Aguarda o término das threads
         for (int t = 0; t < num_threads; t++) {
             if (pthread_join(threads[t], NULL) != 0) {
                 fprintf(stderr, "Erro no join da thread %d\n", t);
             }
         }
 
-        printf("Matriz Resultante:\n");
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                printf("%.2lf ", result[i][j]);
+        printf("Matriz A:\n");
+        for (int i = 0; i < md.n1; i++) {
+            for (int j = 0; j < md.m1; j++) {
+                printf("%.2lf ", md.matriz1[i][j]);
             }
             printf("\n");
         }
 
-        free(td);
-        for (int i = 0; i < n; i++) {
-            free(result[i]);
+        printf("Matriz B:\n");
+        for (int i = 0; i < md.n2; i++) {
+            for (int j = 0; j < md.m2; j++) {
+                printf("%.2lf ", md.matriz2[i][j]);
+            }
+            printf("\n");
         }
-        free(result);
+
+        printf("Matriz Resultante:\n");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                printf("%.2lf ", md.result[i][j]);
+            }
+            printf("\n");
+        }
+
+        // Liberação de memória
+        free(td);
+        free(threads);
+        for (int i = 0; i < n; i++) {
+            free(md.matriz1[i]);
+        }
+        free(md.matriz1);
+        for (int i = 0; i < md.n2; i++) {
+            free(md.matriz2[i]);
+        }
+        free(md.matriz2);
+
+        for (int i = 0; i < n; i++) {
+            free(md.result[i]);
+        }
+        free(md.result);
     }
 
     fclose(input);
-    free(threads);
-    for (int c = 0; c < num_iter; c++) {
-        for (int i = 0; i < md[c].n1; i++) {
-            free(md[c].matriz1[i]);
-        }
-        free(md[c].matriz1);
-        for (int i = 0; i < md[c].n2; i++) {
-            free(md[c].matriz2[i]);
-        }
-        free(md[c].matriz2);
-    }
-    free(md);
     return 0;
 }
