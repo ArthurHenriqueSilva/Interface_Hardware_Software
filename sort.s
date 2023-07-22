333300fec4153717d6101408578c52de9b4781c3
arthur <arthur@Ubuntu.myguest.virtualbox.org>
2023-07-22 18:07:41 -0300

    loop externo com problema

--- sort.s
+++ sort.s
@@ -16,7 +16,7 @@ main:
     // argumento 1 para fopen
     mov rdi, r13
     // argumento 2 para fopen
-    lea rsi, [rip + readmode] 
+    lea rsi, [rip + readmode]
     // abrir arquivo_entrada
     call fopen@plt
     // guarda ponteiro do arquivo
@@ -28,29 +28,10 @@ main:
     // argumento 3 para fscanf
     lea rdx, [rbp - 16]
     call fscanf@plt
-    // argumento 1 para printf
     lea rdi, [rip + output_one_intformat]
-    // argumento 2 para printf (pega valor da pilha)
-    mov rsi, [rbp -  16]
+    mov rsi, [rbp - 16]
     call printf@plt
-    loop_externo_init:
-        mov rcx, 0
-    loop_externo:
-        cmp rcx, [rbp - 16]
-        je done
-        lea rdi, [rip + output_one_intformat]
-        mov rsi, rcx
-        call printf@plt
-        inc rcx
-        jmp loop_externo
-    done:
-        //zera  rax
-        xor rax, rax
-        //reseta pilha
-rbp - 8]
-    call fscanf@plt
-
-    mov r15, [rbp - 8]
+    mov r15, [rbp - 16]
     // for(i = 0; i < r15; i++)
     loop_externo_init:
         // i = 0
@@ -61,8 +42,8 @@ rbp - 8]
         je done
 
         // Imprime o valor de rcx
-        mov rdi, rcx
-        lea rsi, [rip + output_one_intformat]
+        lea rdi, [rip + output_one_intformat]
+        mov rsi, rcx
         call printf@plt
 
         // i++
@@ -71,7 +52,6 @@ rbp - 8]
 
     done:
         xor rax, rax
->>>>>>> fa620b251e3422a622a0731b6c7a1620852d18e1
         mov rsp, rbp
         pop rbp
         ret
