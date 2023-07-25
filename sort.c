00ac5dda875956aae31ee880a84bd113b1e48e30
Arthur Henrique Silva de Lima Arthur <115894179+ArthurHenriqueSilva@users.noreply.github.com>
2023-07-25 16:27:47 -0300

    Create sort.c

---
+++ sort.c
@@ -0,0 +1,12 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[]){
+  FILE* input = fopen(argv[1], 'r');
+  FILE* output = fopen(argv[2], 'w');
+
+  int num_loop_externo;
+  fscanf(input, "%d", num_loop_externo);
+  printf("%d\n", num_loop_externo);
+}
